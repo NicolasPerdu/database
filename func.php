cd977940c3c1d251601e4a14f40cf65b775f1560
Nicolas Perdu <nicolas.perdu@epfl.ch>
2014-05-06 20:35:31 +0200

    A Simple query example

--- func.php
+++ func.php
@@ -1,7 +1,37 @@
 <?php
 
+function connexion() {
+	$username = '';
+	$password = '';
+	$host = 'localhost/XE';
+	return oci_connect($username, $password, $host);
+}
+
 function page() {
 	echo '<p>Content of the database</p>';
+	
+	$conn = connexion();
+	
+	if (!$conn) {
+		$e = oci_error();
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+	
+	$req = 'SELECT * FROM employees';
+	
+	$stid = oci_parse($conn, $req);
+	oci_execute($stid);
+
+	echo "<table>\n";
+	
+	while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+		echo "<tr>\n";
+		foreach ($row as $item) {
+			echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+		}
+		echo "</tr>\n";
+	}
+	echo "</table>\n";
 }
 
 ?>
